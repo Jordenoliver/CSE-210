e18ad285c20302ce623995ac515e8d7385ed8303
Jordenoliver <oli22011@byui.edu>
2023-05-26 10:14:42 -0700

    foundations

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,70 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+       
+
+        Comment com1 = new Comment();
+        com1._username = "benover";
+        com1._text = "Had a good laugh";
+
+        Comment com2 = new Comment();
+        com2._username = "seashanties123";
+        com2._text = "could have better music";
+
+        Comment com3 = new Comment();
+        com3._username = "brucelee20";
+        com3._text = "more fighting please";
+        
+        Video vid1 = new Video();
+        vid1._title = "funny dog videos";
+        vid1._author = "puppygowoof";
+        vid1._length = 1000;
+        vid1._comments.Add(com1);
+        vid1._comments.Add(com2);
+        vid1._comments.Add(com3);
+        vid1.DisplayVideoInfo();
+
+
+        Comment com4 = new Comment();
+        com4._username = "MiddleNameDanger";
+        com4._text = "ya, you gonna die one day";
+
+        Comment com5 = new Comment();
+        com5._username = "Carnut";
+        com5._text = "why, just why";
+
+        Comment com6 = new Comment();
+        com6._username = "user1835648923";
+        com6._text = "That's so dangerous";
+        
+        Video vid2 = new Video();
+        vid2._title = "telling my mexican mother her salsa is not spicy";
+        vid2._author = "thiswasamistake";
+        vid2._length = 70;
+        vid2._comments.Add(com4);
+        vid2._comments.Add(com5);
+        vid2._comments.Add(com6);
+        vid2.DisplayVideoInfo();
+
+        Comment com7 = new Comment();
+        com7._username = "autobodyshop";
+        com7._text = "We will see you here again soon I see";
+
+        Comment com8 = new Comment();
+        com8._username = "Carnut";
+        com8._text = "why, just why";
+
+        Comment com9 = new Comment();
+        com9._username = "user8739248345";
+        com9._text = "WHAT HAVE YOU DONE";
+        
+        Video vid3 = new Video();
+        vid3._title = "2023 Corvette getting its second accident";
+        vid3._author = "CorvetteDriver";
+        vid3._length = 30;
+        vid3._comments.Add(com7);
+        vid3._comments.Add(com8);
+        vid3._comments.Add(com9);
+        vid3.DisplayVideoInfo();
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        
+        Product product1 = new Product("Apples", "GH57H", 1.5, 70);
+        Product product2 = new Product("Lettuce", "SOH98", 2.33, 90);
+        Product product3 = new Product("Mangoes", "GP483", 10.2, 10);
+        Product product4 = new Product("watermelons", "26FH7", 5.8, 2000);
+        Product product5 = new Product("Yogurt", "8KE36", 4.2, 40);
+        Product product6 = new Product("Milk", "KQW4Y", 2.3, 50);
+        
+
+
+        Address address1 = new Address("123 Main St", "New York City", "NY", "USA");
+        string newAddress1 = address1.GetAddress();
+        string newCountry1 = address1.GetCountry();
+        Customer customer1 = new Customer("Fred the math teacher", $"{newAddress1}", $"{newCountry1}");
+
+        Address address2 = new Address("321 side St", "San Diago", "CA", "USA");
+        string newAddress2 = address2.GetAddress();
+        string newCountry2 = address2.GetCountry();
+        Customer customer2 = new Customer("Bob the math teacher", $"{newAddress2}", $"{newCountry2}");
+
+        Order order1 = new Order();
+        order1._customer = customer1.DisplayCustomerInfo();
+        order1._shippingCost = customer1.GetShippingCost();
+        order1._products.Add(product1);
+        order1._products.Add(product2);
+
+        Order order2 = new Order();
+        order2._customer = customer2.DisplayCustomerInfo();
+        order2._shippingCost = customer2.GetShippingCost();
+        order2._products.Add(product3);
+        order2._products.Add(product4);
+        order2._products.Add(product5);
+        order2._products.Add(product6);
+
+        order1.DisplayTotalPrice();
+        Console.WriteLine();
+        order1.DisplayShippingLabel();
+        Console.WriteLine();
+        order1.DisplayPackingLabel();
+        Console.WriteLine();
+
+        order2.DisplayTotalPrice();
+        Console.WriteLine();
+        order2.DisplayShippingLabel();
+        Console.WriteLine();
+        order2.DisplayPackingLabel();
+        Console.WriteLine();
+
     }
 }
